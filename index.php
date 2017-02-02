17aa0c04268defe5b086cbb6211e9cba359a5e3a
khatangatao <khatan@mail.ru>
2017-02-02 19:46:33 +0700

    Начало создания главной страницы гостевой книги

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+echo <input type="text">
+
+<p><b>Каким браузером в основном пользуетесь:</b><Br>
+<input type="radio" name="browser" value="ie"> Internet Explorer<Br>
+<input type="radio" name="browser" value="opera"> Opera<Br>
+<input type="radio" name="browser" value="firefox"> Firefox<Br>
+</p>
+<p>Комментарий<Br>
+<textarea name="comment" cols="40" rows="3"></textarea></p>
+<p><input type="submit" value="Отправить">
+<input type="reset" value="Очистить"></p>
+
+
+
+phpinfo(32);
+?>
