f2c616c24978e3b2eb5ffeb819d63ebd599d64e3
khatangatao <khatan@mail.ru>
2017-02-08 13:11:50 +0700

    Создана форма гостевой книги с записью в БД. Вывод данных не доделал: просто распечатан ассоциативный массив.

--- index.php
+++ index.php
@@ -1,15 +1,31 @@
 <?php
-echo <input type="text">
+require_once('config/config.php');
 
-<p><b>Каким браузером в основном пользуетесь:</b><Br>
-<input type="radio" name="browser" value="ie"> Internet Explorer<Br>
-<input type="radio" name="browser" value="opera"> Opera<Br>
-<input type="radio" name="browser" value="firefox"> Firefox<Br>
-</p>
-<p>Комментарий<Br>
-<textarea name="comment" cols="40" rows="3"></textarea></p>
-<p><input type="submit" value="Отправить">
-<input type="reset" value="Очистить"></p>
+echo '<form method = "POST">';
+echo '<p><b>Ваше имя:</b><Br>';
+echo '<input type="text" name = "name">';
+echo '<p><b>Ваш емайл:</b><Br>';
+echo '<input type="text" name = "email">';
+echo '</p>';
+echo '<p>Комментарий<Br>';
+echo '<textarea name="comment" cols="40" rows="3"></textarea></p>';
+echo '<p><input type="submit" value="Отправить">';
+echo '<input type="reset" value="Очистить"></p>';
+echo '</form>';
+
+// echo $name . '<br>';
+// echo $email . '<br>';
+// echo $comment . '<br>';
+
+//Блок вывода комментариев из базы данных
+//Готовим sql-запрос
+$query = 'SELECT * from myshop.feedback ORDER BY id_feedback DESC';
+$feedbackblock = getAssocResult($query);
+
+echo '<hr>';
+echo '<pre>';
+print_r($feedbackblock);
+echo '</pre>';
 
 
 
